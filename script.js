a9b4cd2859d0bf8716c825ab796b4234affe18b0
Glitch (hello-webpage) <none>
2020-11-08 02:48:47 +0000

    🎗🏑 Checkpoint ./index.html:21940894/2073 ./script.js:21940894/813 ./.glitch-assets:21940894/355

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+d3.csv("https://cdn.glitch.com/a3e24eba-b378-48e5-a881-609f19dd60d6%2Fdriving.csv?v=1604803460994",d3.autoType)
+  .then(data=>{
+  data=data;
+  console.log(data);
+  
+  const height = 500;
+  const width= 650;
+  
+  const svg = d3.select("body")
+  .append("svg")
+  .attr("viewBox", [0,0, width, height]) ;
+  
+  
+  
+  
+});
