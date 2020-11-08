90e65ec835b4cb0ad4b17a3ec4db44e855703d24
Glitch (hello-webpage) <none>
2020-11-08 03:00:35 +0000

    🛥✌️ Checkpoint ./index.html:21940894/30 ./style.css:21940894/168 ./script.js:21940894/75

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 d3.csv("https://cdn.glitch.com/a3e24eba-b378-48e5-a881-609f19dd60d6%2Fdriving.csv?v=1604803460994",d3.autoType)
   .then(data=>{
   data=data;
-  console.log(data);
+  const sorted= data.sort((a,b)=>a.year-b.year);
+  console.log(sorted);
   
   const height = 500;
   const width= 650;
